0d87c75339c449c5f0739114d7caa5a5aab4589f
mit <mit@WSN-MIT-T480S.is.intarsys.de>
2019-01-15 18:24:34 +0100

    Initial release

---
+++ SpringIssue6423/src/main/java/de/intarsys/spring/issue/Main.java
@@ -0,0 +1,35 @@
+package de.intarsys.spring.issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		TransactionAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		SecurityFilterAutoConfiguration.class,
+		OAuth2ResourceServerAutoConfiguration.class,
+		OAuth2ClientAutoConfiguration.class
+})
+@ImportResource({ "classpath*:spring-beans.xml" })
+public class Main extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Main.class);
+	}
+
+}
